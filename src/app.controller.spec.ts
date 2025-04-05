755f3706332b0eb87ce858a07a7285172890043b
Alviss Raghnall <isiomastewart@gmail.com>
2025-04-05 08:11:11 +0100

    found itt

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,6 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Transaction } from '@mysten/sui/transactions';
 
+jest.mock('@mysten/sui/transactions');
+jest.mock('@mysten/sui/client');
+jest.mock('@mysten/sui/keypairs/ed25519');
+jest.mock('@mysten/bcs', () => ({
+    fromBase64: jest.fn().mockReturnValue(new Uint8Array([0, 1, 2, 3])),
+}));
+
 describe('AppController', () => {
   console.log(Transaction);
   let appController: AppController;
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Transaction } from '@mysten/sui/transactions';
+import { SuiClient } from '@mysten/sui/client';
+import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
+import { fromBase64 } from '@mysten/sui/utils';
 
 @Controller()
 export class AppController {
